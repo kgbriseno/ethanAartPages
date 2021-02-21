dd09a57bf5732ec0b3535748788dc39faef1c5a5
kgbriseno <kgbriseno@gmail.com>
2021-02-20 20:37:47 -0800

    third commit

--- DRAFTgallery.js
+++ DRAFTgallery.js
@@ -1,6 +1,11 @@
-(function() {
-  $(document).on("click", ".click-to-expand", function() {
-    var imageSrc = $(this).parents(".grid-item").find("img").attr("src");
-    $(".js-modal-image").attr("src", imageSrc);
-  });
-})();
+var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  columnWidth: '.grid-sizer'
+});
+
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry();
+});  
+
